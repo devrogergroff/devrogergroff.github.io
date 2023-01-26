0868c322c4189137dd10ea4533510ac4b2281645
devrogergroff <devrogergroff@outlook.com>
2023-01-26 11:40:05 -0300

    undo

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Full Stack Engineer Experienced Full Stack Engineer with over 5 years of experience.',
+	},
+	{
+		id: 2,
+		bio: 'Excellent reputation for resolving problems and improving customer satisfaction, expert in diverse programming languages.',
+	},
+];
